fefe6629f28b069b1a199ab6cea7605a0160ceef
incoval <102046584+incoval@users.noreply.github.com>
2026-02-19 09:38:13 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/",
+   base: "/per-impact-sim/",
   server: {
     host: "::",
     port: 8080,
